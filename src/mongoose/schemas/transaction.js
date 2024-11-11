410f39ddd8377a0dca2bf6ef3e6343e8d9ff7d4f
Wildan Novaldi <wildanv10@gmail.com>
2024-11-11 20:57:50 +0700

    feat: add Transaction schema

---
+++ src/mongoose/schemas/transaction.js
@@ -0,0 +1,64 @@
+import mongoose from "mongoose";
+
+const transactionSchema = new mongoose.Schema(
+  {
+    user_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    type: {
+      type: String,
+      required: true,
+      enum: ["income", "expense"],
+    },
+    date: {
+      type: Date,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    amount: {
+      type: Number,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    sub_category: {
+      type: String,
+      required: true,
+    },
+    payment_method: {
+      type: String,
+      required: true,
+      // TODO: create CRUD function for Payment Method that embedding in User document
+      enum: [
+        "Cash",
+        "BCA - Virtual Account",
+        "BCA - QRIS",
+        "BCA - Transfer",
+        "BCA BLU - Virtual Account",
+        "BCA BLU - QRIS",
+        "BCA BLU - Transfer",
+        "UOB - QRIS",
+        "UOB - Transfer",
+        "Shopee Pay",
+        "GoPay",
+      ],
+    },
+    notes: {
+      type: String,
+      default: "",
+    },
+  },
+  { timestamps: true },
+);
+
+export const TransactionSchema = mongoose.model(
+  "Transaction",
+  transactionSchema,
+);
