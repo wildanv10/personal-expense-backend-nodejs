270705de1569d588a99bcf9e039eaaa8eddc0991
Wildan Novaldi <wildanv10@gmail.com>
2024-12-01 00:45:51 +0700

    feat(user): add payment_methods column and its values

--- src/mongoose/schemas/user.js
+++ src/mongoose/schemas/user.js
@@ -5,6 +5,7 @@ import mongoose from "mongoose";
 const userSchema = new mongoose.Schema({
   name: String,
   email: { type: String, unique: true },
+  payment_methods: [String],
 });
 
 export const UserSchema = mongoose.model("User", userSchema);
