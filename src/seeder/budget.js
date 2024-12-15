6093546499101eac936b7b57b440b06d9101cec2
Wildan Novaldi <wildanv10@gmail.com>
2024-12-15 20:13:05 +0700

    feat(budget): add budget API
    - add seeder
    - add post route /budget/seeder
    - add get route /budget/period
    - add post route /budget
    - add get route /budget
    - add put route /budget/:budgetId/:budgetDetailId

---
+++ src/seeder/budget.js
@@ -0,0 +1,605 @@
+export const budgetSeeder = [
+  // user: dev.wildanv10@gmail.com
+  {
+    user_id: "672cdc0507e908f6660cf79a",
+    period_month: 11,
+    period_year: 2024,
+    budget_details: [
+      {
+        name: "Salary",
+        type: "income",
+        sub_categories: [
+          { name: "Basic Salary", amount: 0 },
+          { name: "Bonus", amount: 0 },
+          { name: "Overtime", amount: 0 },
+          { name: "Others", amount: 0 },
+        ],
+      },
+      {
+        name: "Investments",
+        type: "income",
+        sub_categories: [
+          { name: "Dividends", amount: 0 },
+          { name: "Capital Gains", amount: 0 },
+          { name: "Others", amount: 0 },
+        ],
+      },
+      {
+        name: "Housing",
+        type: "expense",
+        sub_categories: [
+          { name: "Internet", amount: 0 },
+          { name: "Electricity", amount: 0 },
+          { name: "Supplies", amount: 0 },
+          { name: "Mortgage", amount: 0 },
+          { name: "Others", amount: 0 },
+        ],
+      },
+      {
+        name: "Transport",
+        type: "expense",
+        sub_categories: [
+          { name: "Fuel", amount: 0 },
+          { name: "Maintenance", amount: 0 },
+          { name: "Licensing", amount: 0 },
+          { name: "Loans", amount: 0 },
+          { name: "Others", amount: 0 },
+        ],
+      },
+      {
+        name: "Food",
+        type: "expense",
+        sub_categories: [
+          { name: "Food and Drink", amount: 0 },
+          { name: "Others", amount: 0 },
+        ],
+      },
+      {
+        name: "Personal Care",
+        type: "expense",
+        sub_categories: [
+          { name: "Medical", amount: 0 },
+          { name: "Clothing", amount: 0 },
+          { name: "Health Club", amount: 0 },
+          { name: "Laundry", amount: 0 },
+          { name: "Others", amount: 0 },
+        ],
+      },
+      {
+        name: "Entertainment",
+        type: "expense",
+        sub_categories: [
+          { name: "Youtube", amount: 0 },
+          { name: "Spotify", amount: 0 },
+          { name: "Netflix", amount: 0 },
+          { name: "Mobile Internet", amount: 0 },
+          { name: "Vidio", amount: 0 },
+          { name: "Others", amount: 0 },
+        ],
+      },
+      {
+        name: "Savings",
+        type: "expense",
+        sub_categories: [
+          { name: "Investments", amount: 0 },
+          { name: "Emergency", amount: 0 },
+          { name: "Vacation", amount: 0 },
+          { name: "Pension", amount: 0 },
+          { name: "Others", amount: 0 },
+        ],
+      },
+      {
+        name: "Presents",
+        type: "expense",
+        sub_categories: [
+          { name: "Zakat", amount: 0 },
+          { name: "Charity", amount: 0 },
+          { name: "Present to Parents", amount: 0 },
+          { name: "Others", amount: 0 },
+        ],
+      },
+    ],
+  },
+  {
+    user_id: "672cdc0507e908f6660cf79a",
+    period_month: 12,
+    period_year: 2024,
+    budget_details: [
+      {
+        name: "Salary",
+        type: "income",
+        sub_categories: [
+          { name: "Basic Salary", amount: 0 },
+          { name: "Bonus", amount: 0 },
+          { name: "Overtime", amount: 0 },
+          { name: "Others", amount: 0 },
+        ],
+      },
+      {
+        name: "Investments",
+        type: "income",
+        sub_categories: [
+          { name: "Dividends", amount: 0 },
+          { name: "Capital Gains", amount: 0 },
+          { name: "Others", amount: 0 },
+        ],
+      },
+      {
+        name: "Housing",
+        type: "expense",
+        sub_categories: [
+          { name: "Internet", amount: 0 },
+          { name: "Electricity", amount: 0 },
+          { name: "Supplies", amount: 0 },
+          { name: "Mortgage", amount: 0 },
+          { name: "Others", amount: 0 },
+        ],
+      },
+      {
+        name: "Transport",
+        type: "expense",
+        sub_categories: [
+          { name: "Fuel", amount: 0 },
+          { name: "Maintenance", amount: 0 },
+          { name: "Licensing", amount: 0 },
+          { name: "Loans", amount: 0 },
+          { name: "Others", amount: 0 },
+        ],
+      },
+      {
+        name: "Food",
+        type: "expense",
+        sub_categories: [
+          { name: "Food and Drink", amount: 0 },
+          { name: "Others", amount: 0 },
+        ],
+      },
+      {
+        name: "Personal Care",
+        type: "expense",
+        sub_categories: [
+          { name: "Medical", amount: 0 },
+          { name: "Clothing", amount: 0 },
+          { name: "Health Club", amount: 0 },
+          { name: "Laundry", amount: 0 },
+          { name: "Others", amount: 0 },
+        ],
+      },
+      {
+        name: "Entertainment",
+        type: "expense",
+        sub_categories: [
+          { name: "Youtube", amount: 0 },
+          { name: "Spotify", amount: 0 },
+          { name: "Netflix", amount: 0 },
+          { name: "Mobile Internet", amount: 0 },
+          { name: "Vidio", amount: 0 },
+          { name: "Others", amount: 0 },
+        ],
+      },
+      {
+        name: "Savings",
+        type: "expense",
+        sub_categories: [
+          { name: "Investments", amount: 0 },
+          { name: "Emergency", amount: 0 },
+          { name: "Vacation", amount: 0 },
+          { name: "Pension", amount: 0 },
+          { name: "Others", amount: 0 },
+        ],
+      },
+      {
+        name: "Presents",
+        type: "expense",
+        sub_categories: [
+          { name: "Zakat", amount: 0 },
+          { name: "Charity", amount: 0 },
+          { name: "Present to Parents", amount: 0 },
+          { name: "Others", amount: 0 },
+        ],
+      },
+    ],
+  },
+  {
+    user_id: "672cdc0507e908f6660cf79a",
+    period_month: 1,
+    period_year: 2025,
+    budget_details: [
+      {
+        name: "Salary",
+        type: "income",
+        sub_categories: [
+          { name: "Basic Salary", amount: 0 },
+          { name: "Bonus", amount: 0 },
+          { name: "Overtime", amount: 0 },
+          { name: "Others", amount: 0 },
+        ],
+      },
+      {
+        name: "Investments",
+        type: "income",
+        sub_categories: [
+          { name: "Dividends", amount: 0 },
+          { name: "Capital Gains", amount: 0 },
+          { name: "Others", amount: 0 },
+        ],
+      },
+      {
+        name: "Housing",
+        type: "expense",
+        sub_categories: [
+          { name: "Internet", amount: 0 },
+          { name: "Electricity", amount: 0 },
+          { name: "Supplies", amount: 0 },
+          { name: "Mortgage", amount: 0 },
+          { name: "Others", amount: 0 },
+        ],
+      },
+      {
+        name: "Transport",
+        type: "expense",
+        sub_categories: [
+          { name: "Fuel", amount: 0 },
+          { name: "Maintenance", amount: 0 },
+          { name: "Licensing", amount: 0 },
+          { name: "Loans", amount: 0 },
+          { name: "Others", amount: 0 },
+        ],
+      },
+      {
+        name: "Food",
+        type: "expense",
+        sub_categories: [
+          { name: "Food and Drink", amount: 0 },
+          { name: "Others", amount: 0 },
+        ],
+      },
+      {
+        name: "Personal Care",
+        type: "expense",
+        sub_categories: [
+          { name: "Medical", amount: 0 },
+          { name: "Clothing", amount: 0 },
+          { name: "Health Club", amount: 0 },
+          { name: "Laundry", amount: 0 },
+          { name: "Others", amount: 0 },
+        ],
+      },
+      {
+        name: "Entertainment",
+        type: "expense",
+        sub_categories: [
+          { name: "Youtube", amount: 0 },
+          { name: "Spotify", amount: 0 },
+          { name: "Netflix", amount: 0 },
+          { name: "Mobile Internet", amount: 0 },
+          { name: "Vidio", amount: 0 },
+          { name: "Others", amount: 0 },
+        ],
+      },
+      {
+        name: "Savings",
+        type: "expense",
+        sub_categories: [
+          { name: "Investments", amount: 0 },
+          { name: "Emergency", amount: 0 },
+          { name: "Vacation", amount: 0 },
+          { name: "Pension", amount: 0 },
+          { name: "Others", amount: 0 },
+        ],
+      },
+      {
+        name: "Presents",
+        type: "expense",
+        sub_categories: [
+          { name: "Zakat", amount: 0 },
+          { name: "Charity", amount: 0 },
+          { name: "Present to Parents", amount: 0 },
+          { name: "Others", amount: 0 },
+        ],
+      },
+    ],
+  },
+
+  // user: wildanv10@gmail.com
+  {
+    user_id: "6741acc9fe8d8ffcdbbd0296",
+    period_month: 11,
+    period_year: 2024,
+    budget_details: [
+      {
+        name: "Salary",
+        type: "income",
+        sub_categories: [
+          { name: "Basic Salary", amount: 0 },
+          { name: "Bonus", amount: 0 },
+          { name: "Overtime", amount: 0 },
+          { name: "Others", amount: 0 },
+        ],
+      },
+      {
+        name: "Investments",
+        type: "income",
+        sub_categories: [
+          { name: "Dividends", amount: 0 },
+          { name: "Capital Gains", amount: 0 },
+          { name: "Others", amount: 0 },
+        ],
+      },
+      {
+        name: "Housing",
+        type: "expense",
+        sub_categories: [
+          { name: "Internet", amount: 0 },
+          { name: "Electricity", amount: 0 },
+          { name: "Supplies", amount: 0 },
+          { name: "Mortgage", amount: 0 },
+          { name: "Others", amount: 0 },
+        ],
+      },
+      {
+        name: "Transport",
+        type: "expense",
+        sub_categories: [
+          { name: "Fuel", amount: 0 },
+          { name: "Maintenance", amount: 0 },
+          { name: "Licensing", amount: 0 },
+          { name: "Loans", amount: 0 },
+          { name: "Others", amount: 0 },
+        ],
+      },
+      {
+        name: "Food",
+        type: "expense",
+        sub_categories: [
+          { name: "Food and Drink", amount: 0 },
+          { name: "Others", amount: 0 },
+        ],
+      },
+      {
+        name: "Personal Care",
+        type: "expense",
+        sub_categories: [
+          { name: "Medical", amount: 0 },
+          { name: "Clothing", amount: 0 },
+          { name: "Health Club", amount: 0 },
+          { name: "Laundry", amount: 0 },
+          { name: "Others", amount: 0 },
+        ],
+      },
+      {
+        name: "Entertainment",
+        type: "expense",
+        sub_categories: [
+          { name: "Youtube", amount: 0 },
+          { name: "Spotify", amount: 0 },
+          { name: "Netflix", amount: 0 },
+          { name: "Mobile Internet", amount: 0 },
+          { name: "Vidio", amount: 0 },
+          { name: "Others", amount: 0 },
+        ],
+      },
+      {
+        name: "Savings",
+        type: "expense",
+        sub_categories: [
+          { name: "Investments", amount: 0 },
+          { name: "Emergency", amount: 0 },
+          { name: "Vacation", amount: 0 },
+          { name: "Pension", amount: 0 },
+          { name: "Others", amount: 0 },
+        ],
+      },
+      {
+        name: "Presents",
+        type: "expense",
+        sub_categories: [
+          { name: "Zakat", amount: 0 },
+          { name: "Charity", amount: 0 },
+          { name: "Present to Parents", amount: 0 },
+          { name: "Others", amount: 0 },
+        ],
+      },
+    ],
+  },
+  {
+    user_id: "6741acc9fe8d8ffcdbbd0296",
+    period_month: 12,
+    period_year: 2024,
+    budget_details: [
+      {
+        name: "Salary",
+        type: "income",
+        sub_categories: [
+          { name: "Basic Salary", amount: 0 },
+          { name: "Bonus", amount: 0 },
+          { name: "Overtime", amount: 0 },
+          { name: "Others", amount: 0 },
+        ],
+      },
+      {
+        name: "Investments",
+        type: "income",
+        sub_categories: [
+          { name: "Dividends", amount: 0 },
+          { name: "Capital Gains", amount: 0 },
+          { name: "Others", amount: 0 },
+        ],
+      },
+      {
+        name: "Housing",
+        type: "expense",
+        sub_categories: [
+          { name: "Internet", amount: 0 },
+          { name: "Electricity", amount: 0 },
+          { name: "Supplies", amount: 0 },
+          { name: "Mortgage", amount: 0 },
+          { name: "Others", amount: 0 },
+        ],
+      },
+      {
+        name: "Transport",
+        type: "expense",
+        sub_categories: [
+          { name: "Fuel", amount: 0 },
+          { name: "Maintenance", amount: 0 },
+          { name: "Licensing", amount: 0 },
+          { name: "Loans", amount: 0 },
+          { name: "Others", amount: 0 },
+        ],
+      },
+      {
+        name: "Food",
+        type: "expense",
+        sub_categories: [
+          { name: "Food and Drink", amount: 0 },
+          { name: "Others", amount: 0 },
+        ],
+      },
+      {
+        name: "Personal Care",
+        type: "expense",
+        sub_categories: [
+          { name: "Medical", amount: 0 },
+          { name: "Clothing", amount: 0 },
+          { name: "Health Club", amount: 0 },
+          { name: "Laundry", amount: 0 },
+          { name: "Others", amount: 0 },
+        ],
+      },
+      {
+        name: "Entertainment",
+        type: "expense",
+        sub_categories: [
+          { name: "Youtube", amount: 0 },
+          { name: "Spotify", amount: 0 },
+          { name: "Netflix", amount: 0 },
+          { name: "Mobile Internet", amount: 0 },
+          { name: "Vidio", amount: 0 },
+          { name: "Others", amount: 0 },
+        ],
+      },
+      {
+        name: "Savings",
+        type: "expense",
+        sub_categories: [
+          { name: "Investments", amount: 0 },
+          { name: "Emergency", amount: 0 },
+          { name: "Vacation", amount: 0 },
+          { name: "Pension", amount: 0 },
+          { name: "Others", amount: 0 },
+        ],
+      },
+      {
+        name: "Presents",
+        type: "expense",
+        sub_categories: [
+          { name: "Zakat", amount: 0 },
+          { name: "Charity", amount: 0 },
+          { name: "Present to Parents", amount: 0 },
+          { name: "Others", amount: 0 },
+        ],
+      },
+    ],
+  },
+  {
+    user_id: "6741acc9fe8d8ffcdbbd0296",
+    period_month: 1,
+    period_year: 2025,
+    budget_details: [
+      {
+        name: "Salary",
+        type: "income",
+        sub_categories: [
+          { name: "Basic Salary", amount: 0 },
+          { name: "Bonus", amount: 0 },
+          { name: "Overtime", amount: 0 },
+          { name: "Others", amount: 0 },
+        ],
+      },
+      {
+        name: "Investments",
+        type: "income",
+        sub_categories: [
+          { name: "Dividends", amount: 0 },
+          { name: "Capital Gains", amount: 0 },
+          { name: "Others", amount: 0 },
+        ],
+      },
+      {
+        name: "Housing",
+        type: "expense",
+        sub_categories: [
+          { name: "Internet", amount: 0 },
+          { name: "Electricity", amount: 0 },
+          { name: "Supplies", amount: 0 },
+          { name: "Mortgage", amount: 0 },
+          { name: "Others", amount: 0 },
+        ],
+      },
+      {
+        name: "Transport",
+        type: "expense",
+        sub_categories: [
+          { name: "Fuel", amount: 0 },
+          { name: "Maintenance", amount: 0 },
+          { name: "Licensing", amount: 0 },
+          { name: "Loans", amount: 0 },
+          { name: "Others", amount: 0 },
+        ],
+      },
+      {
+        name: "Food",
+        type: "expense",
+        sub_categories: [
+          { name: "Food and Drink", amount: 0 },
+          { name: "Others", amount: 0 },
+        ],
+      },
+      {
+        name: "Personal Care",
+        type: "expense",
+        sub_categories: [
+          { name: "Medical", amount: 0 },
+          { name: "Clothing", amount: 0 },
+          { name: "Health Club", amount: 0 },
+          { name: "Laundry", amount: 0 },
+          { name: "Others", amount: 0 },
+        ],
+      },
+      {
+        name: "Entertainment",
+        type: "expense",
+        sub_categories: [
+          { name: "Youtube", amount: 0 },
+          { name: "Spotify", amount: 0 },
+          { name: "Netflix", amount: 0 },
+          { name: "Mobile Internet", amount: 0 },
+          { name: "Vidio", amount: 0 },
+          { name: "Others", amount: 0 },
+        ],
+      },
+      {
+        name: "Savings",
+        type: "expense",
+        sub_categories: [
+          { name: "Investments", amount: 0 },
+          { name: "Emergency", amount: 0 },
+          { name: "Vacation", amount: 0 },
+          { name: "Pension", amount: 0 },
+          { name: "Others", amount: 0 },
+        ],
+      },
+      {
+        name: "Presents",
+        type: "expense",
+        sub_categories: [
+          { name: "Zakat", amount: 0 },
+          { name: "Charity", amount: 0 },
+          { name: "Present to Parents", amount: 0 },
+          { name: "Others", amount: 0 },
+        ],
+      },
+    ],
+  },
+];
